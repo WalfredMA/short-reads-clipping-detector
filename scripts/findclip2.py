d0534746eb522bc19a89a86ae5b512fbed0ab0ec
Wangfei MA <Wangfei.MA@ucsf.edu>
2019-12-07 06:10:46 -0800

    Update findclip2.py

--- scripts/findclip2.py
+++ scripts/findclip2.py
@@ -8,13 +8,6 @@ import sys
 import getopt
 import math
 
-
-
-
-opts,args=getopt.getopt(sys.argv[1:],"f:")
-for op, value in opts:
-	if op=='-f':
-		inputfile=value
 		
 
 def findclippingchance(chrom):
@@ -129,6 +122,11 @@ def main():
 	
 	
 if __name__=='__main__':
+	
+	opts,args=getopt.getopt(sys.argv[1:],"f:")
+	for op, value in opts:
+		if op=='-f':
+			inputfile=value
 	
 	main()
 
